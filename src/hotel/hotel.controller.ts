ce3a321a45eb8172fd2493800acf83dbd7049f14
Евгений Сишненко <sishnenko86@mail.ru>
2023-01-04 12:18:22 +0300

    добавил метод добавления гостиниц

--- src/hotel/hotel.controller.ts
+++ src/hotel/hotel.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/common/jwt.auth.guard';
+import { AddHotelParamsDTO } from './dto/add.hotel.params.dto';
+import { HotelService } from './hotel.service';
 
 @Controller('hotel')
-export class HotelController {}
+export class HotelController {
+  constructor(private hotelService: HotelService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('/api/admin/hotels')
+  async addHotel(@Body() addHotelDTO: AddHotelParamsDTO) {
+    console.log(addHotelDTO);
+
+    const hotel = await this.hotelService.addHotel(addHotelDTO);
+    return {
+      id: hotel._id,
+      title: hotel.title,
+      description: hotel.description,
+    };
+  }
+}
--- src/hotel/hotel.service.ts
+++ src/hotel/hotel.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Connection, Model } from 'mongoose';
+import { AddHotelParamsDTO } from './dto/add.hotel.params.dto';
+import { Hotel, HotelDocument } from './schemas/hotel.schemas';
 
 @Injectable()
-export class HotelService {}
+export class HotelService {
+  constructor(
+    @InjectModel(Hotel.name) private HotelModel: Model<HotelDocument>,
+    @InjectConnection() private connection: Connection,
+  ) {}
+  async addHotel(addHotelDTO: AddHotelParamsDTO) {
+    const newHotel = new this.HotelModel(addHotelDTO);
+    return await newHotel.save();
+  }
+}
