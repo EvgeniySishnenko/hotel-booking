528a5977cf46204c3e3bb0fc7b6cd9c44f7f23cd
Евгений Сишненко <sishnenko86@mail.ru>
2023-01-04 16:19:57 +0300

    добавил getHotels

--- src/hotel/hotel.controller.ts
+++ src/hotel/hotel.controller.ts
@@ -1,22 +1,52 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/common/jwt.auth.guard';
 import { AddHotelParamsDTO } from './dto/add.hotel.params.dto';
 import { HotelService } from './hotel.service';
+import { IFindSearchParams } from './interfaces/find-search.params.interface';
 
 @Controller('hotel')
 export class HotelController {
   constructor(private hotelService: HotelService) {}
 
+  /** еще guard нужен- 403 - если роль пользователя не admin. */
   @UseGuards(JwtAuthGuard)
+  @UsePipes(ValidationPipe)
   @Post('/api/admin/hotels')
   async addHotel(@Body() addHotelDTO: AddHotelParamsDTO) {
-    console.log(addHotelDTO);
+    try {
+      const hotel = await this.hotelService.addHotel(addHotelDTO);
+      return {
+        id: hotel._id,
+        title: hotel.title,
+        description: hotel.description,
+      };
+    } catch (error) {
+      return error;
+    }
+  }
 
-    const hotel = await this.hotelService.addHotel(addHotelDTO);
-    return {
-      id: hotel._id,
-      title: hotel.title,
-      description: hotel.description,
-    };
+  //   interface SearchHotelParams {
+  //   limit: number;
+  //   offset: number;
+  //   title: string;
+  // }
+  @Get('/api/admin/hotels')
+  async getHotels(@Query() params: IFindSearchParams) {
+    try {
+      const hotels = await this.hotelService.getHotels(params);
+      return hotels;
+    } catch (error) {
+      return error;
+    }
   }
 }
--- src/hotel/hotel.service.ts
+++ src/hotel/hotel.service.ts
@@ -2,16 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { Connection, Model } from 'mongoose';
 import { AddHotelParamsDTO } from './dto/add.hotel.params.dto';
+import { IFindSearchParams } from './interfaces/find-search.params.interface';
 import { Hotel, HotelDocument } from './schemas/hotel.schemas';
 
 @Injectable()
 export class HotelService {
   constructor(
-    @InjectModel(Hotel.name) private HotelModel: Model<HotelDocument>,
+    @InjectModel(Hotel.name) private hotelModel: Model<HotelDocument>,
     @InjectConnection() private connection: Connection,
   ) {}
   async addHotel(addHotelDTO: AddHotelParamsDTO) {
-    const newHotel = new this.HotelModel(addHotelDTO);
+    const newHotel = new this.hotelModel(addHotelDTO);
     return await newHotel.save();
+  }
+  async getHotels(params: IFindSearchParams) {
+    const skip = Number(params.offset) || 0;
+    const limit = Number(params.limit) || 6;
+
+    return this.hotelModel.find().skip(skip).limit(limit).exec();
   }
 }
