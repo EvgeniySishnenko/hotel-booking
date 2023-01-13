156b21e3cbcd08113bf70fd124a59cf5f4944387
Евгений Сишненко <sishnenko86@mail.ru>
2023-01-13 08:53:02 +0300

    добавил метод addReservation

--- src/reservations/reservations.controller.ts
+++ src/reservations/reservations.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/common/jwt.auth.guard';
+import { CurrentUser } from 'src/auth/decorator/current.user.decorator';
+import { TID } from 'src/hotel-room/interfaces/hotel.room.interfaces';
+import { User } from 'src/users/schemas/user.schemas';
+import { ReservationDto } from './dto/reservation.dto';
+import { ReservationsService } from './reservations.service';
 
 @Controller('reservations')
-export class ReservationsController {}
+export class ReservationsController {
+  constructor(private reservationsService: ReservationsService) {}
+
+  /** 403 - если роль пользователя не client; */
+  @UseGuards(JwtAuthGuard)
+  @Post('/api/client/reservations')
+  async addReservation(
+    @Body() data: ReservationDto,
+    @CurrentUser() user: User & { _id: TID },
+  ) {
+    try {
+      return await this.reservationsService.addReservation(data, user._id);
+    } catch (error) {
+      return error;
+    }
+  }
+}
--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Connection, Model } from 'mongoose';
+import { HotelRoomService } from 'src/hotel-room/hotel-room.service';
+import { TID } from 'src/hotel-room/interfaces/hotel.room.interfaces';
+import { HotelRoom } from 'src/hotel-room/schemas/hotelRoom.schemas';
+import { Role } from 'src/users/enums/roles.enum';
+import { ReservationDto } from './dto/reservation.dto';
+import { Reservations } from './schemas/reservations.schemas';
 
 @Injectable()
-export class ReservationsService {}
+export class ReservationsService {
+  constructor(
+    @InjectModel(Reservations.name)
+    private reservationsModel: Model<Reservations>,
+    private hotelRoomService: HotelRoomService,
+    @InjectConnection() private connection: Connection,
+  ) {}
+
+  async addReservation(data: ReservationDto, id: TID) {
+    try {
+      const hotelRoom = await this.hotelRoomService.getHotelRoom(
+        data.hotelRoom,
+      );
+
+      if (!hotelRoom || !hotelRoom.isEnabled) throw new BadRequestException();
+
+      const newReservation = new this.reservationsModel({
+        ...data,
+        roomId: data.hotelRoom,
+        userId: id,
+        hotelId: hotelRoom.hotel,
+      });
+      await newReservation.save();
+      return newReservation;
+    } catch (error) {
+      return error;
+    }
+  }
+}
